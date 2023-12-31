e68e0de19ca927a09ee7399e29766e4224143152
Yaara Segev <Yaara.Segev@agamimp.net>
2023-12-31 15:44:29 +0200

    ex run script

---
+++ client/src/ApiServices/Ex_Script.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/extensions */
+// import unitsData from './units.js';
+// import officesData from './offices.js';
+// import departmentsData from './departments.js';
+
+// console.log(unitsData);
+// console.log(officesData);
+// console.log(departmentsData);
+
+const args = process.argv[process.argv.length - 1];
+// const item = unitsData.find((s) => s['_id']['$oid'] === args);
+console.log(args);
+
+// console.log('args' + args);
